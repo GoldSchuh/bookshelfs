a8185c89c60f3a61ed80366b724cf7adccba2b35
watson <107920501+JeCheeseSmith@users.noreply.github.com>
2026-01-23 13:54:06 +0100

    Annotated & generated openapi.json specifications

--- lib/Controller/BooksController.php
+++ lib/Controller/BooksController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace OCA\Bookshelfs\Controller;
 
-use Exception;
 use OCA\Bookshelfs\Db\BookMapper;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\Attribute\ApiRoute;
@@ -24,92 +23,102 @@ class BooksController extends OCSController {
 		parent::__construct($appName, $request);
 	}
 
+
 	/**
-	 * @return DataResponse
-	 */
-	// TODO change like this
-	/**
-	 * Get all books for the current user
+	 * Return a list of all books of the current user
 	 *
-	 * @return DataResponse<Http::STATUS_OK, array<Book>, array{}>|DataResponse<Http::STATUS_BAD_REQUEST, array{error: string}, array{}>
+	 * @return DataResponse<Http::STATUS_OK, list<array{id: int, title: string , author: string, position: int , url: string, file: int, colour: string , pattern: int, height: int}> , array{}> | DataResponse<Http::STATUS_BAD_REQUEST, list<string>, array{}>
 	 *
-	 * 200: Books returned successfully
-	 * 400: Bad request
+	 * @response 200: All user books returned successfully
+	 * @response 400: Bad request
 	 */
 	#[NoAdminRequired]
-	#[ApiRoute(verb: 'GET', url: '/api/{apiVersion}/books', requirements: [
-		'apiVersion' => 'v1',
-	])]
+	#[ApiRoute(
+		verb: 'GET',
+		url: '/api/v1/books',
+	)]
 	public function getUserBooks(): DataResponse {
 		try {
 			return new DataResponse($this->bookMapper->getBooksOfUser($this->userId));
-		} catch (Exception|Throwable $e) {
+		} catch (Throwable $e) {
 			return new DataResponse(['error' => $e->getMessage()], Http::STATUS_BAD_REQUEST);
 		}
 	}
 
 	/**
-	 * @param string $title
-	 * @param string $author
-	 * @param int $position
-	 * @param string $url
-	 * @param int $file
-	 * @param string $colour
-	 * @param int $pattern
-	 * @param int $height
-	 * @return DataResponse
+	 * Create a new book for the current user
+	 *
+	 * @param string $title Title of the book
+	 * @param string $author Author of the book
+	 * @param int $position Position of the book in the shelf (array index)
+	 * @param string $url File ID of the book cover image
+	 * @param int $file File ID of the (e-)book file
+	 * @param string $colour Color of the book
+	 * @param int $pattern Pattern of the book
+	 * @param int $height Height of the book
+	 *
+	 * @return DataResponse<Http::STATUS_OK, array{id: int, title: string , author: string, position: int , url: string, file: int, colour: string , pattern: int, height: int} , array{}> | DataResponse<Http::STATUS_BAD_REQUEST, list<string>, array{}>
+	 *
+	 * @response 200: Created and returned the book successfully
+	 * @response 400: Bad request
 	 */
 	#[NoAdminRequired]
-	#[ApiRoute(verb: 'POST', url: '/api/{apiVersion}/books', requirements: [
-		'apiVersion' => 'v1',
-	])]
+	#[ApiRoute(verb: 'POST', url: '/api/v1/books')]
 	public function addUserBook(string $title, string $author, int $position, string $url, int $file, string $colour, int $pattern, int $height): DataResponse {
 		try {
 			$book = $this->bookMapper->createBook($this->userId, $title, $author, $position, $url, $file, $colour, $pattern, $height);
 			return new DataResponse($book);
-		} catch (Exception|Throwable $e) {
+		} catch (Throwable $e) {
 			return new DataResponse(['error' => $e->getMessage()], Http::STATUS_BAD_REQUEST);
 		}
 	}
 
 	/**
-	 * @param int $id
-	 * @param string|null $title
-	 * @param string|null $author
-	 * @param int|null $position
-	 * @param string|null $url
-	 * @param int|null $file
-	 * @param string|null $colour
-	 * @param int|null $pattern
-	 * @param int|null $height
-	 * @return DataResponse
+	 * Update the parameters of an existing book (id) of the current user
+	 *
+	 * @param int $id Identifier of the book to edit
+	 * @param string|null $title New title of the book
+	 * @param string|null $author New author of the book
+	 * @param int|null $position New position of the book in the shelf (array index)
+	 * @param string|null $url New image (file ID) for the book cover image
+	 * @param int|null $file New e-book (file ID) for the book
+	 * @param string|null $colour New color of the book
+	 * @param int|null $pattern New pattern of the book
+	 * @param int|null $height New height of the book
+	 *
+	 * @return DataResponse<Http::STATUS_OK, array{id: int, title: string , author: string, position: int , url: string, file: int, colour: string , pattern: int, height: int} , array{}> | DataResponse<Http::STATUS_BAD_REQUEST, list<string>, array{}>
+	 *
+	 * @response 200: Updated and returned the updated book successfully
+	 * @response 400: Bad request
 	 */
 	#[NoAdminRequired]
-	#[ApiRoute(verb: 'PUT', url: '/api/{apiVersion}/books/{id}', requirements: [
-		'apiVersion' => 'v1',
-	])]
+	#[ApiRoute(verb: 'PUT', url: '/api/v1/books/{id}')]
 	public function editUserBook(int $id, ?string $title = null, ?string $author = null, ?int $position = null, ?string $url = null, ?int $file = null, ?string $colour = null, ?int $pattern = null, ?int $height = null): DataResponse {
 		try {
 			$book = $this->bookMapper->updateBook($id, $this->userId, $title, $author, $position, $url, $file, $colour, $pattern, $height);
 			return new DataResponse($book);
-		} catch (Exception|Throwable $e) {
+		} catch (Throwable $e) {
 			return new DataResponse(['error' => $e->getMessage()], Http::STATUS_BAD_REQUEST);
 		}
 	}
 
 	/**
-	 * @param int $id
-	 * @return DataResponse
+	 * Delete an existing book (id) of the current user
+	 *
+	 * @param int $id Identifier of the book to delete
+	 *
+	 * @return DataResponse<Http::STATUS_OK, array{id: int, title: string , author: string, position: int , url: string, file: int, colour: string , pattern: int, height: int} , array{}> | DataResponse<Http::STATUS_BAD_REQUEST, list<string>, array{}>
+	 *
+	 * @response 200: Deleted and returned the deleted book successfully
+	 * @response 400: Bad request
 	 */
 	#[NoAdminRequired]
-	#[ApiRoute(verb: 'DELETE', url: '/api/{apiVersion}/books/{id}', requirements: [
-		'apiVersion' => 'v1',
-	])]
+	#[ApiRoute(verb: 'DELETE', url: '/api/v1/books/{id}')]
 	public function deleteUserBook(int $id): DataResponse {
 		try {
 			$book = $this->bookMapper->deleteBook($id, $this->userId);
 			return new DataResponse($book);
-		} catch (Exception|Throwable $e) {
+		} catch (Throwable $e) {
 			return new DataResponse(['error' => $e->getMessage()], Http::STATUS_BAD_REQUEST);
 		}
 	}
